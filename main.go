8ff33e88ff0ba9c5cda156f2c998289193889621
jonathanmarpaung <jonathangilbertmarpaung@gmail.com>
2026-05-13 00:09:32 +0700

    Big revision, adding some utilites, power model, log model, and modules table.

--- main.go
+++ main.go
@@ -2,140 +2,195 @@ package main
 
 import "fmt"
 
-// ---------------- STRUCT ---------------------
+// --------------------= GLOBAL =--------------------
 
-type Time struct {
-	Hour   int
-	Minute int
-	Second int
+const MaxLogs int = 999
+
+// --------------------= UTILITY =--------------------
+
+func digitsToString(number int) string {
+	var lastDigit int
+	var result string
+
+	if number == 0 {
+		result = ""
+	} else {
+		lastDigit = number % 10
+		result = digitsToString(number/10) + string(byte(lastDigit)+'0')
+	}
+
+	return result
 }
 
-type PowerLog struct {
-	DeviceName     string
-	DeviceLocation string
-	Power          float64
-	Duration       Time
-}
+func IntToStr(number int) string {
+	var isNegative bool
+	var result string
 
-// ---------------- GLOBAL ---------------------
-
-const NMAX int = 100
-
-type TabListMenu [NMAX]string
-type TabPowerLog [NMAX]PowerLog
-
-// ---------------- SUB PROGRAM ---------------------
-
-// Displays the menu list and returns a valid user selection.
-func GetMenuSelection(list TabListMenu, n int) int {
-	// TODO: improve menu style
-	var i, selection int
-	var valid bool
-
-	valid = false
-
-	for !valid {
-		// display the menu list with index starting from 1 to n
-		for i = 0; i < n; i++ {
-			fmt.Printf("%d. %s\n", (i + 1), list[i])
+	if number == 0 {
+		result = "0"
+	} else {
+		if number < 0 {
+			isNegative = true
+			number = -number
 		}
 
-		// get user selection
-		fmt.Printf("Select menu: ")
-		fmt.Scan(&selection)
+		result = digitsToString(number)
 
-		// validate selection
-		if selection >= 1 && selection <= n {
-			valid = true
+		if isNegative {
+			result = "-" + result
 		}
 	}
 
-	return selection
+	return result
 }
 
-/**
- * I.S. TODO
- * F.S. TODO
- */
-func InsertLog(log *TabPowerLog, n *int) {
-	// TODO: handle if array have been in maximum capacity
-	fmt.Printf("Device Name: ")
-	fmt.Scan(&log[*n].DeviceName)
+func FloatToStr(value float64) string {
+	var result, precisionStr string
+	var valueInt, precisionInt int
+	var isNegative bool
 
-	fmt.Printf("Device Location: ")
-	fmt.Scan(&log[*n].DeviceLocation)
-
-	fmt.Printf("Power Used (watt): ")
-	fmt.Scan(&log[*n].Power)
-
-	fmt.Printf("Duration (Hour): ")
-	fmt.Scan(&log[*n].Duration.Hour)
-
-	fmt.Printf("Duration (Minute): ")
-	fmt.Scan(&log[*n].Duration.Minute)
-
-	fmt.Printf("Duration (Second): ")
-	fmt.Scan(&log[*n].Duration.Second)
-
-	// adding one to inform new data has been added
-	*n = *n + 1
-}
-
-/**
- * I.S. array filled with log and n as length log in array
- * F.S. showing log in array as table
- */
-func ShowLog(log TabPowerLog, n int) {
-	// TODO: improve table style
-	var i int
-
-	fmt.Printf("%-3s %-15s %-20s %-13s %s\n",
-		"ID", "Device Name", "Device Location", "Power (Watt)", "Duration")
-
-	for i = 0; i < n; i++ {
-		fmt.Printf("%-3d %-15s %-20s %-13.2f %d hours %d minutes %d seconds\n",
-			i, log[i].DeviceName, log[i].DeviceLocation, log[i].Power,
-			log[i].Duration.Hour, log[i].Duration.Minute, log[i].Duration.Second)
+	if value < 0 {
+		isNegative = true
+		value = -value
 	}
+
+	valueInt = int(value)
+	precisionInt = int((value - float64(valueInt)) * 100)
+
+	if precisionInt == 0 {
+		precisionStr = "00"
+	} else {
+		if precisionInt < 10 {
+			precisionStr = "0" + digitsToString(precisionInt)
+		} else {
+			precisionStr = digitsToString(precisionInt)
+		}
+	}
+
+	result = IntToStr(valueInt) + "." + precisionStr
+
+	if isNegative {
+		result = "-" + result
+	}
+
+	return result
 }
 
-// ---------------- MAIN ---------------------
+// --------------------= DURATION =--------------------
+
+type Duration struct {
+	hours   int
+	minutes int
+	seconds int
+}
+
+func NewDuration(hours int, minutes int, seconds int) Duration {
+	var duration Duration
+
+	duration.hours = hours
+	duration.minutes = minutes
+	duration.seconds = seconds
+
+	return duration
+}
+
+func DurationToStr(duration Duration) string {
+	var result string
+
+	if duration.hours > 0 {
+		result = result + IntToStr(duration.hours) + " Jam "
+	}
+
+	if duration.minutes > 0 {
+		result = result + IntToStr(duration.minutes) + " Menit "
+	}
+
+	result = result + IntToStr(duration.seconds) + " Detik"
+
+	return result
+}
+
+// --------------------= POWER =--------------------
+
+type Power float64
+
+func PowerToStr(power Power) string {
+	return FloatToStr(float64(power)) + " Watt"
+}
+
+// --------------------= LOG =--------------------
+
+type Log struct {
+	deviceName     string
+	deviceLocation string
+	power          Power
+	duration       Duration
+}
+
+type Logs struct {
+	list [MaxLogs]Log
+	n    int
+}
+
+func AddLog(logs *Logs, deviceName string, deviceLocation string, power Power, duration Duration) {
+	var log Log
+
+	log.deviceName = deviceName
+	log.deviceLocation = deviceLocation
+	log.power = power
+	log.duration = duration
+
+	logs.list[logs.n] = log
+
+	logs.n = logs.n + 1
+}
+
+// --------------------= TABLE =--------------------
+
+func PrintTableHead() {
+	fmt.Println("╭─────┬──────────────────────────┬─────────────────────────┬───────────────────────────────────────────────────┬─────────────────╮")
+	fmt.Println("│ No. │      Nama Perangkat      │     Lokasi Perangkat    │                  Durasi Pemakaian                 │ Komsumsi Energi │")
+}
+
+func PrintTableSeparator() {
+	fmt.Println("├─────┼──────────────────────────┼─────────────────────────┼───────────────────────────────────────────────────┼─────────────────┤")
+}
+
+func PrintTableFoot() {
+	fmt.Println("╰─────┴──────────────────────────┴─────────────────────────┴───────────────────────────────────────────────────┴─────────────────╯")
+}
+
+func PrintLogTable(logs Logs) {
+	var index int
+	var currentLog Log
+
+	PrintTableHead()
+
+	for index = 1; index <= logs.n; index++ {
+		PrintTableSeparator()
+
+		currentLog = logs.list[index-1]
+
+		fmt.Printf("│ %-3d │ %-24s │ %-23s │ %-49s │ %-15s │\n",
+			index,
+			currentLog.deviceName,
+			currentLog.deviceLocation,
+			DurationToStr(currentLog.duration),
+			PowerToStr(currentLog.power),
+		)
+	}
+
+	PrintTableFoot()
+}
+
+// --------------------= MAIN =--------------------
 
 func main() {
-	var running bool
-	var log TabPowerLog
-	var nLog int
-	var menuList TabListMenu
-	var nMenuList, selection int
+	var logs Logs
 
-	// initialize menu
-	nMenuList = 5
-	menuList[0] = "Show data"
-	menuList[1] = "Insert data"
-	menuList[2] = "Replace data"
-	menuList[3] = "Delete data"
-	menuList[4] = "Exit"
+	AddLog(&logs, "Laptop", "Kamar_Tidur", 123.23, NewDuration(1, 20, 10))
+	AddLog(&logs, "Kipas_Angin", "Ruang_Tamu", 45.50, NewDuration(4, 0, 0))
+	AddLog(&logs, "Televisi", "Ruang_Keluarga", 110.00, NewDuration(2, 30, 0))
 
-	// initialize log as empty array
-	nLog = 0
-
-	// run the program
-	running = true
-
-	for running {
-		selection = GetMenuSelection(menuList, nMenuList)
-
-		switch selection {
-		case 1:
-			ShowLog(log, nLog)
-		case 2:
-			InsertLog(&log, &nLog)
-		case 3:
-			// TODO: Create a function to handle replace data
-		case 4:
-			// TODO: Create a function to handle delete data
-		case 5:
-			running = false
-		}
-	}
+	PrintLogTable(logs)
 }
